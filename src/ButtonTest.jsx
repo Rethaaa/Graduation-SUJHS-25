adc9abf9343e4c599bac9ed771db8e3a40c1f745
Retha <aretha.hartonoo@gmail.com>
2025-05-16 12:19:35 +0700

    committed

---
+++ src/ButtonTest.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import {Button} from '@mui/material';
+
+export default function ButtonTest() {
+  return (
+    <Button variant="contained" color='secondary'>Hello world</Button>
+  );
+}
---
+++ src/Theme.jsx
@@ -0,0 +1,22 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+
+export default function Theme(props) {
+    const themeOptions = createTheme({
+      palette: {
+        primary: {
+          main: '#002A45',
+          dark: '#011B2F',
+          light: '#003F66',
+        },
+        secondary: {
+          main: '#FFB915',
+          light: '#FFD358'
+        },
+      },
+    });
+    return (
+        <ThemeProvider theme={themeOptions}>
+            {props.children}
+        </ThemeProvider>
+    )
+}
