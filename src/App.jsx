a5ddca6b73afbea4b662c70be62b55efcbda8fbe
jjem4a <jemimallagan10@gmail.com>
2025-05-19 13:00:42 +0700

    navbar udah selesai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import * as React from 'react';
 import Theme from './Theme'
 import ButtonTest from './ButtonTest'
+import Button2 from './Button2.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <Theme>
       <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
     </Theme>
   )
 }
