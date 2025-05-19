4fd3dccf164dd2ee507bbf36e2dabb6aead67bbe
nataliaviolaa <natalia.nugroho@yahoo.com>
2025-05-19 12:57:00 +0700

    navbar udh selesai

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import Theme from './Theme'
 import ButtonTest from './ButtonTest'
+import Button2 from './Button2'
 import './App.css'
 
 export default function App() {
   return (
     <Theme>
       <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <ButtonTest></ButtonTest>
+      <Button2></Button2>
+      <Button2></Button2>
+      <Button2></Button2>
     </Theme>
   )
 }
